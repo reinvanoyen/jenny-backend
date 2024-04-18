2244fd6dba125c6917490f5a28eda5ec7853d644
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-18 20:26:27 +0200

    feat: viezerik command

--- app/Slack/RegisterHandler.php
+++ app/Slack/RegisterHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Slack;
 
+use App\Models\Word;
 use Spatie\SlashCommand\Request;
 use Spatie\SlashCommand\Response;
 use Spatie\SlashCommand\Handlers\BaseHandler;
@@ -10,13 +11,38 @@ class RegisterHandler extends BaseHandler
 {
     public function canHandle(Request $request): bool
     {
-        return str_starts_with($request->text, 'viezerik');
+        return $request->command === 'viezerik';
     }
 
     public function handle(Request $request): Response
     {
-        $argument = substr($request->text, strlen('viezerik'));
+        $word = strtolower($request->text);
+        $segments = explode($word, ' ');
 
-        return $this->respondToSlack("Je zei {$argument}!");
+        $randomWord = Word::inRandomOrder()->limit(1)->get();
+        $randomWordValue = ($randomWord ? $randomWord->word : 'krentenbaard');
+
+        // Check if it's one word first
+        if (count($segments) > 1) {
+            return $this->respondToSlack('Eén woord, stukske '.$randomWordValue.'!');
+        }
+
+        // Check if it already exists
+        $wordModel = Word::where('word', $word)->get();
+
+        if ($wordModel) {
+            $wordModel->rating = $wordModel->rating + 1;
+            $wordModel->save();
+
+            return $this->respondToSlack('We hadden "'.$word.'" al. Beetje bij de pinken blijven he, mislukten '.$randomWordValue.'! Kheb het dan maar een extra puntje gegeven, wa moet ik anders doen?!');
+        }
+
+        // Create the word
+        $wordModel = new Word();
+        $wordModel->word = $word;
+        $wordModel->rating = 1;
+        $wordModel->save();
+
+        return $this->respondToSlack($word. ' ofwa? Ieeuw. Me wa zijt ge eigenlijk bezig? Maar allee, tis goe, kheb het opgeslaan in mijn '.$randomWordValue.'..euh ik bedoel database.');
     }
 }
