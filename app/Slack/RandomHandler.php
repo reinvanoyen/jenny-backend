20620a3662c3ef740ae75ab9bfaeba5073a2a259
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-18 20:59:52 +0200

    random

---
+++ app/Slack/RandomHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Slack;
+
+use App\Models\Word;
+use Illuminate\Support\Str;
+use Spatie\SlashCommand\Request;
+use Spatie\SlashCommand\Response;
+use Spatie\SlashCommand\Handlers\BaseHandler;
+
+class RandomHandler extends BaseHandler
+{
+    public function canHandle(Request $request): bool
+    {
+        return Str::startsWith($request->text, 'vuilaard');
+    }
+
+    public function handle(Request $request): Response
+    {
+        $randomWord = Word::inRandomOrder()->first();
+        $randomWordValue = ($randomWord ? $randomWord->word : 'krentenbaard');
+
+        return $this->respondToSlack($randomWordValue . ' (Reting: '.$randomWord->rating.' vieze punten)')
+            ->displayResponseToEveryoneOnChannel();
+    }
+}
--- config/laravel-slack-slash-command.php
+++ config/laravel-slack-slash-command.php
@@ -32,6 +32,7 @@ return [
     'handlers' => [
         //add your own handlers here
         \App\Slack\RegisterHandler::class,
+        \App\Slack\RandomHandler::class,
 
         //this handler will display instructions on how to use the various commands.
         Spatie\SlashCommand\Handlers\Help::class,
