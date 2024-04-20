3f9102ced00d58049048cc37a1ad82434599c532
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-20 21:29:36 +0200

    progress

--- app/Slack/Translator.php
+++ app/Slack/Translator.php
@@ -8,10 +8,10 @@ class Translator
 {
     public function translate(string $input): string
     {
-        $word = Word::latest()->value('word');
-        $noun = Word::inRandomOrder()->first()->value('word');
-        $verb = Word::inRandomOrder()->first()->value('word');
-        $random = Word::inRandomOrder()->first()->value('word');
+        $word = Word::latest()->first()->get()->value('word');
+        $noun = Word::inRandomOrder()->first()->get()->value('word');
+        $verb = Word::inRandomOrder()->first()->get()->value('word');
+        $random = Word::inRandomOrder()->first()->get()->value('word');
 
         return str_replace([
             '{word}',
