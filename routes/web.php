eb500ac2b8e2a7cfe8949bd1e4d1d9f29cbb9e25
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-21 23:58:44 +0200

    progress

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,19 @@ Route::post('slack/interactivity', function (\Illuminate\Http\Request $request) 
 
     $payload = json_decode($request->input('payload'), true);
     $responseUrl = $payload['response_url'];
+    $votedWord = $payload['actions']['selected_option']['wondvocht'] ?? null;
+
+    $wordModel = \App\Models\Word::where('word', $votedWord)->first();
+
+    if ($wordModel) {
+        $wordModel->rating = $wordModel->rating + 1;
+        $wordModel->save();
+    }
 
     $response = \Illuminate\Support\Facades\Http::post($responseUrl, [
         'text' => 'Merci vadsigaardje!',
         'replace_original' => 'true',
+        'delete_original' => 'true',
     ]);
 
     return response()->json([
