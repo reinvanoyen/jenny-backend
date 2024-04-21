4abdbae044c6ec5abe4c1543732d4518f84f7383
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-22 00:07:03 +0200

    progress

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('slack/interactivity', function (\Illuminate\Http\Request $request) {
-    \Illuminate\Support\Facades\Log::debug($request);
 
     $payload = json_decode($request->input('payload'), true);
     $responseUrl = $payload['response_url'];
     $votedWord = $payload['actions']['selected_option']['value'] ?? null;
+
+    \Illuminate\Support\Facades\Log::debug($payload);
+    \Illuminate\Support\Facades\Log::debug($votedWord);
 
     $wordModel = \App\Models\Word::where('word', $votedWord)->first();
 
     if ($wordModel) {
         $wordModel->rating = $wordModel->rating + 1;
         $wordModel->save();
-    }
 
-    $response = \Illuminate\Support\Facades\Http::post($responseUrl, [
-        'text' => 'Merci vadsigaardje, uw stem voor "'.$wordModel->word.'" is binnen! 🤢',
-        'replace_original' => 'true',
-        'delete_original' => 'true',
-        'type' => 'in_channel',
-    ]);
+        \Illuminate\Support\Facades\Http::post($responseUrl, [
+            'text' => 'Merci vadsigaardje, uw stem voor "'.$wordModel->word.'" is binnen! 🤢',
+            'replace_original' => 'true',
+            'delete_original' => 'true',
+            'type' => 'in_channel',
+        ]);
+    }
 
     return response()->json([
         'status' => 'ok',
