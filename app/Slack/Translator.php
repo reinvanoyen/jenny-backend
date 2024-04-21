dad4d20b6849387dc6fcaf5030d1a8cc7640d5b0
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-21 15:24:40 +0200

    randuser

--- app/Slack/Translator.php
+++ app/Slack/Translator.php
@@ -2,6 +2,7 @@
 
 namespace App\Slack;
 
+use App\Models\Author;
 use App\Models\Word;
 
 class Translator
@@ -12,6 +13,7 @@ class Translator
         $noun = Word::inRandomOrder()->first()->word;
         $verb = Word::inRandomOrder()->first()->word;
         $random = Word::inRandomOrder()->first()->word;
+        $randomUser = Author::inRandomOrder()->first()->name;
         $count = Word::count();
 
         return str_replace([
@@ -20,12 +22,14 @@ class Translator
             '{verb}',
             '{random}',
             '{count}',
+            '{randuser}',
         ], [
             $word,
             $noun,
             $verb,
             $random,
             $count,
+            $randomUser,
         ], $input);
     }
 }
