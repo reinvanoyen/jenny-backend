3207ff4dcafbab5ca546a432c7b7391c3741a2aa
Rein Van Oyen <reinvanoyen@gmail.com>
2024-04-22 13:10:18 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,6 @@ Route::post('slack/interactivity', function (\Illuminate\Http\Request $request) 
 
         \Illuminate\Support\Facades\Http::post($responseUrl, [
             'text' => 'Merci vadsigaardje, uw stem voor "'.$wordModel->word.'" is binnen! 🤢',
-            'replace_original' => 'true',
         ]);
 
         if (isset($author) && $author) {
